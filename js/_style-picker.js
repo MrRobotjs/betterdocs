38832aa2c698e6198c63e8d1b8bf3dd14c1f1a2b
Christopher <39032787+MrRobotjs@users.noreply.github.com>
2018-09-25 19:34:53 -0700

    Update
    -Made navbar stick when scrolling and more responsive

--- js/_style-picker.js
+++ js/_style-picker.js
@@ -10,3 +10,14 @@
 		$parent.children(".style[data-item-id=" + id + "]").show();
 	});
 })();
+$(function(){
+	var navbar = $('header>.container');
+	
+	$(window).scroll(function(){
+		if($(window).scrollTop() <= 40){
+			navbar.removeClass('navbar-scroll');
+		} else {
+			navbar.addClass('navbar-scroll');
+		}
+	});
+});
